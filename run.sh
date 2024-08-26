bc173edd59d88bc94062048a925fae9b95a20945
Zanie Blue <contact@zanie.dev>
2024-08-26 15:46:03 -0500

    Add more documentation (#1)

--- run.sh
+++ run.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env sh
+#
+# Build and run the example Docker image.
+#
+# Mounts the local project directory to reflect a common development workflow.
+#
+# The `docker run` command uses the following options:
+#
+#   --rm                        Remove the container after exiting
+#   --volume .:/app             Mount the current directory to `/app` so code changes don't require an image rebuild
+#   --volume /app/.venv         Mount the virtual environment separately, so the developer's environment doesn't end up in the container
+#   -it $(docker build -q .)    Build the image, then use it as a run target
+#   $@                          Pass any arguments to the container
 
-docker run --rm --volume .:/app --volume /app/.venv -it $(docker build -q .) $@
+docker run \
+    --rm \
+    --volume .:/app \
+    --volume /app/.venv \
+    -it $(docker build -q .) \
+    $@
