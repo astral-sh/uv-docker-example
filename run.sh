1838149cce4bfadecf135179d69efc03c5b70952
Zanie Blue <contact@zanie.dev>
2024-08-26 19:47:33 -0500

    Add Docker compose example (#7)

--- run.sh
+++ run.sh
@@ -9,6 +9,7 @@
 #   --rm                        Remove the container after exiting
 #   --volume .:/app             Mount the current directory to `/app` so code changes don't require an image rebuild
 #   --volume /app/.venv         Mount the virtual environment separately, so the developer's environment doesn't end up in the container
+#   --expose                    Expose the web server port 8000 to the host
 #   -it $(docker build -q .)    Build the image, then use it as a run target
 #   $@                          Pass any arguments to the container
 
@@ -22,6 +23,7 @@ docker run \
     --rm \
     --volume .:/app \
     --volume /app/.venv \
+    --publish 8000:8000 \
     $INTERACTIVE \
     $(docker build -q .) \
     "$@"
