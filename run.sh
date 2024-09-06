13bbe9815f920be1254d012f05cdd4aeb3d62327
Ethan Holz <ethan.holz@omsf.io>
2024-09-06 10:15:55 -0500

    docs: fix docs to match command line argument

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@
 #   --rm                        Remove the container after exiting
 #   --volume .:/app             Mount the current directory to `/app` so code changes don't require an image rebuild
 #   --volume /app/.venv         Mount the virtual environment separately, so the developer's environment doesn't end up in the container
-#   --expose                    Expose the web server port 8000 to the host
+#   --publish 8000:8000         Expose the web server port 8000 to the host
 #   -it $(docker build -q .)    Build the image, then use it as a run target
 #   $@                          Pass any arguments to the container
 
